225277ef2bb29e400f000a1e60024035ee02f101
John Doe <johndoe@email.com>
2023-06-01 18:04:25 -0400

    Adding click events

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,6 +1,10 @@
 import {useState} from 'react';
+import femaleProfile from './images/femaleProfile.jpg';
+import maleProfile from './images/maleProfile.jpg';
 
 const Employees = () => {
+
+    const [selectedTeam, setTeam] = useState("TeamB");
 
     const [employees, setEmployees] = useState([
         {
@@ -86,15 +90,60 @@ const Employees = () => {
         designation: "Graphic Designer",
         gender: "female",
         teamName: "TeamD"}
-    ])
+    ]);
+
+    function handleTeamSelectionChange(event){
+        setTeam(event.target.value);
+    }
+
+    function handleEmployeeCardClick(event){
+        const transforemdEmployees = employees.map(
+            (employee) => employee.id === parseInt(event.currentTarget.id)?(employee.teamName === selectedTeam)?{...employee, teamName:''}:{...employee, teamName: selectedTeam}:employee
+        );
+        setEmployees(transforemdEmployees);
+    }
 
     return (
-        <main>
+        <main className="container">
+                <div classNmae="row justify-content-center mt-3 mb-3">
+                    <div className="col-6">
+                        <select className="form-select form-selet-lg" value={selectedTeam} onChange={handleTeamSelectionChange}>
+                            <option value="TeamA">TeamA</option>
+                            <option value="TeamB">TeamB</option>
+                            <option value="TeamC">TeamC</option>
+                            <option value="TeamD">TeamD</option>
+                        </select>
+                    </div>
+                </div>
+
+            <div className="row justify-content-center mt-3 mb-3">
+                <div className="col-8">
+                    <div className="card-collection">
             {
                 employees.map((employee) => (
-                    <p>{employee.fullName}</p> 
+                    <div id={employee.id} className="card m-2" style={{ cursor:"pointer" }} onClick={handleEmployeeCardClick}>
+
+                        {
+                        (employee.gender === 'male')?<img src={maleProfile} 
+                        className="card-img-top"/>
+                        :<img src={femaleProfile} 
+                        className="card-img-top"/>
+                        }
+
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                Full Name: {employee.fullName}
+                            </h5>
+                            <p className="card-text">
+                                <b>Designation:</b>{employee.designation}
+                            </p>
+                        </div>
+                    </div>
                 ))
             }
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
