44df23ce93c02d2adefabb6f66572d903aa0b1ad
John Doe <johndoe@email.com>
2023-06-06 03:15:19 -0400

    - included route to handle invalid paths - added code for grouped team members component - fixed slight UI issue when there is 1 team member - cody tidy; abstracting functionality into its own components - included media query

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Header from './Header';
 import Employees from './Employees';
 import Footer from './Footer';
 import GroupedTeamMembers from './GroupedTeamMembers';
-import Nav from './Nav'
+import Nav from './Nav';
+import NotFound from './NotFound';
 import {useState, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -135,7 +136,9 @@ function App() {
           >
             
             </Route>
-            <Route path="/GroupedTeamMembers" element={<GroupedTeamMembers/>}>
+            <Route path="/GroupedTeamMembers" element={<GroupedTeamMembers employees = {employees} selectedTeam={selectedTeam} setTeam={setTeam} />}>
+            </Route>
+            <Route path="*" element={<NotFound />}>
             </Route>
         </Routes>
         <Footer/>
